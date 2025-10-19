ba2ee934a1574039d68ebc06228dbe2bc287163c
rmzvr <rmzvr@proton.me>
2025-10-19 17:51:25 +0200

    refactor(ex02): update PmergeMe header file to improve method signatures and include necessary headers

--- ex02/includes/PmergeMe.hpp
+++ ex02/includes/PmergeMe.hpp
@@ -2,6 +2,9 @@
 
 #include <list>
 #include <vector>
+#include <chrono>
+#include <string>
+#include <iomanip>
 #include <iostream>
 #include <algorithm>
 
@@ -17,24 +20,24 @@ class PmergeMe
 		void	sort( VectorSequence & sequence );
 		void	sort( ListSequence & sequence );
 
-		void	printNumbersVector( std::vector<int> const & sequence );
-
 	private:
-		static constexpr int MAX_JACOBSTHAL_INDEX = 33;
-		static constexpr int JACOBSTHAL_START_INDEX = 3;
-		static constexpr int MIN_SIZE_FOR_SORTING = 2;
+		static constexpr int	MAX_JACOBSTHAL_INDEX = 33;
+		static constexpr int	JACOBSTHAL_START_INDEX = 3;
+		static constexpr int	MIN_SIZE_FOR_SORTING = 2;
 
 		void	_mergeInsertionSort( VectorSequence & sequence );
 		void	_swapTwoElements( VectorSequence & sequence );
 		void	_splitToChains( VectorSequence const & sequence, VectorSequence & larger_elements, VectorSequence & smaller_elements );
+		void	_generateJacobsthalSequence( VectorSequence & sequence, int n, int c );
+		void	_insertWithJacobsthalSequence( VectorSequence & larger_elements, VectorSequence & smaller_elements );
 		void	_binaryInsertion( VectorSequence & sequence, int number );
 
 
 		void	_mergeInsertionSort( ListSequence & sequence );
 		void	_swapTwoElements( ListSequence& sequence );
-		void	_splitToChains( ListSequence const & sequence, VectorSequence & larger_elements, VectorSequence & smaller_elements );
+		void	_splitToChains( ListSequence const & sequence, ListSequence & larger_elements, ListSequence & smaller_elements );
+		void	_generateJacobsthalSequence( ListSequence & sequence, int n, int c );
+		void	_insertWithJacobsthalSequence( ListSequence & larger_elements, ListSequence & smaller_elements );
 		void	_binaryInsertion( ListSequence& sequence, int number );
 
-		void			_insertWithJacobsthalSequence( VectorSequence & larger_elements, VectorSequence & smaller_elements );
-		VectorSequence	_getJacobsthalSequence( int n, int c );
 };
