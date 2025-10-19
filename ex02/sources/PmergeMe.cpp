644fd762e3f3941c3453ebd36deef81474ab773f
rmzvr <rmzvr@proton.me>
2025-10-19 17:51:33 +0200

    refactor(ex02): remove unused printNumbersVector method and update sorting logic for VectorSequence and ListSequence

--- ex02/sources/PmergeMe.cpp
+++ ex02/sources/PmergeMe.cpp
@@ -16,15 +16,6 @@ void PmergeMe::sort( ListSequence & sequence )
 	this->_mergeInsertionSort(sequence);
 }
 
-void PmergeMe::printNumbersVector( std::vector<int> const & sequence )
-{
-	for (auto const & i : sequence)
-	{
-		std::cout << i << " ";
-	}
-	std::cout << std::endl;
-}
-
 void PmergeMe::_mergeInsertionSort( VectorSequence & sequence )
 {
 	size_t	number_size = sequence.size();
@@ -38,8 +29,8 @@ void PmergeMe::_mergeInsertionSort( VectorSequence & sequence )
 		return ;
 	}
 
-	std::vector<int>	larger_elements;
-	std::vector<int>	smaller_elements;
+	VectorSequence	larger_elements;
+	VectorSequence	smaller_elements;
 	
 	this->_splitToChains(sequence, larger_elements, smaller_elements);
 	
@@ -65,9 +56,9 @@ void PmergeMe::_splitToChains( VectorSequence const & sequence, VectorSequence &
 	larger_elements.reserve(pairs);
 	smaller_elements.reserve(pairs + (sequence.size() % 2));
 
-	for (std::vector<int>::const_iterator curr_it = sequence.begin(); curr_it < sequence.end(); curr_it += 2)
+	for (VectorSequence::const_iterator curr_it = sequence.begin(); curr_it < sequence.end(); curr_it += 2)
 	{
-		std::vector<int>::const_iterator	next_it = std::next(curr_it);
+		VectorSequence::const_iterator	next_it = std::next(curr_it);
 
 		if (next_it == sequence.end())
 		{
@@ -85,6 +76,51 @@ void PmergeMe::_splitToChains( VectorSequence const & sequence, VectorSequence &
 
 	if (sequence.size() % 2 != 0)
 		smaller_elements.push_back(sequence.back());
+}
+
+void	PmergeMe::_generateJacobsthalSequence( PmergeMe::VectorSequence & sequence, int n, int c )
+{
+	int	prev1 = 1;
+	int	prev2 = 0;
+
+	for (int i = PmergeMe::JACOBSTHAL_START_INDEX; i <= n; ++i)
+	{
+		int	curr = prev1 + 2 * prev2;
+		if (curr >= c)
+			break;
+		prev2 = prev1;
+		prev1 = curr;
+		sequence.push_back(curr);
+	}
+}
+
+void PmergeMe::_insertWithJacobsthalSequence( VectorSequence & larger_elements, VectorSequence & smaller_elements )
+{
+	size_t			smaller_elements_size = smaller_elements.size();
+	
+	VectorSequence	jacobsthal_sequence;
+	this->_generateJacobsthalSequence(jacobsthal_sequence, PmergeMe::MAX_JACOBSTHAL_INDEX, smaller_elements_size);
+	
+	size_t			jacobsthal_sequence_size = jacobsthal_sequence.size();
+
+	for (size_t i = 0; i < jacobsthal_sequence_size; ++i)
+	{
+		int	range_start_index = jacobsthal_sequence[i] - 1;
+		int	range_end_index;
+		if (i + 1 >= jacobsthal_sequence_size)
+		{
+			range_end_index = smaller_elements_size;
+		}
+		else
+		{
+			range_end_index = jacobsthal_sequence[i + 1] - 1;
+		}
+
+		for (int j = range_end_index - 1; j >= range_start_index; --j)
+		{
+			this->_binaryInsertion(larger_elements, smaller_elements[j]);
+		}
+	}
 }
 
 void PmergeMe::_binaryInsertion( VectorSequence & sequence, int number )
@@ -107,8 +143,8 @@ void PmergeMe::_mergeInsertionSort( ListSequence & sequence )
 		return ;
 	}
 
-	std::vector<int>	larger_elements;
-	std::vector<int>	smaller_elements;
+	ListSequence	larger_elements;
+	ListSequence	smaller_elements;
 	
 	this->_splitToChains(sequence, larger_elements, smaller_elements);
 	
@@ -127,16 +163,11 @@ void PmergeMe::_swapTwoElements( ListSequence & sequence )
 	}
 }
 
-void PmergeMe::_splitToChains( ListSequence const & sequence, VectorSequence & larger_elements, VectorSequence & smaller_elements )
+void PmergeMe::_splitToChains( ListSequence const & sequence, ListSequence & larger_elements, ListSequence & smaller_elements )
 {
-	// Explain why it doing it
-	size_t	pairs = sequence.size() / 2;
-	larger_elements.reserve(pairs);
-	smaller_elements.reserve(pairs + (sequence.size() % 2));
-
-	for (std::list<int>::const_iterator curr_it = sequence.begin(); curr_it != sequence.end(); )
+	for (ListSequence::const_iterator curr_it = sequence.begin(); curr_it != sequence.end(); )
 	{
-		std::list<int>::const_iterator	next_it = std::next(curr_it);
+		ListSequence::const_iterator	next_it = std::next(curr_it);
 
 		if (next_it == sequence.end())
 		{
@@ -158,44 +189,10 @@ void PmergeMe::_splitToChains( ListSequence const & sequence, VectorSequence & l
 		smaller_elements.push_back(sequence.back());
 }
 
-void PmergeMe::_binaryInsertion( ListSequence & sequence, int number )
+void	PmergeMe::_generateJacobsthalSequence( PmergeMe::ListSequence & sequence, int n, int c )
 {
-	ListSequence::iterator	it = std::lower_bound(sequence.begin(), sequence.end(), number);
-
-	sequence.insert(it, number);
-}
-
-void PmergeMe::_insertWithJacobsthalSequence( VectorSequence & larger_elements, VectorSequence & smaller_elements )
-{
-	size_t				smaller_elements_size = smaller_elements.size();
-	std::vector<int>	jacobsthal_sequence = this->_getJacobsthalSequence(PmergeMe::MAX_JACOBSTHAL_INDEX, smaller_elements_size);
-	size_t				jacobsthal_sequence_size = jacobsthal_sequence.size();
-
-	for (size_t i = 0; i < jacobsthal_sequence_size; ++i)
-	{
-		int	range_start_index = jacobsthal_sequence[i] - 1;
-		int	range_end_index;
-		if (i + 1 >= jacobsthal_sequence_size)
-		{
-			range_end_index = smaller_elements_size;
-		}
-		else
-		{
-			range_end_index = jacobsthal_sequence[i + 1] - 1;
-		}
-
-		for (int j = range_end_index - 1; j >= range_start_index; --j)
-		{
-			this->_binaryInsertion(larger_elements, smaller_elements[j]);
-		}
-	}
-}
-
-PmergeMe::VectorSequence PmergeMe::_getJacobsthalSequence( int n, int c )
-{
-	std::vector<int>	sequence;
-	int					prev1 = 1;
-	int					prev2 = 0;
+	int	prev1 = 1;
+	int	prev2 = 0;
 
 	for (int i = PmergeMe::JACOBSTHAL_START_INDEX; i <= n; ++i)
 	{
@@ -206,5 +203,47 @@ PmergeMe::VectorSequence PmergeMe::_getJacobsthalSequence( int n, int c )
 		prev1 = curr;
 		sequence.push_back(curr);
 	}
-	return sequence;
+}
+
+void PmergeMe::_insertWithJacobsthalSequence( ListSequence & larger_elements, ListSequence & smaller_elements )
+{
+	size_t			smaller_elements_size = smaller_elements.size();
+
+	ListSequence	jacobsthal_sequence;
+	this->_generateJacobsthalSequence(jacobsthal_sequence, PmergeMe::MAX_JACOBSTHAL_INDEX, smaller_elements_size);
+
+	size_t			jacobsthal_sequence_size = jacobsthal_sequence.size();
+
+	for (size_t i = 0; i < jacobsthal_sequence_size; ++i)
+	{
+		ListSequence::iterator jac_it_start = jacobsthal_sequence.begin();
+		std::advance(jac_it_start, i);
+		int	range_start_index = *jac_it_start - 1;
+		
+		int	range_end_index;
+		if (i + 1 >= jacobsthal_sequence_size)
+		{
+			range_end_index = smaller_elements_size;
+		}
+		else
+		{
+			ListSequence::iterator jac_it_end = jacobsthal_sequence.begin();
+			std::advance(jac_it_end, i + 1);
+			range_end_index = *jac_it_end - 1;
+		}
+
+		for (int j = range_end_index - 1; j >= range_start_index; --j)
+		{
+			ListSequence::iterator small_it = smaller_elements.begin();
+			std::advance(small_it, j);
+			this->_binaryInsertion(larger_elements, *small_it);
+		}
+	}
+}
+
+void PmergeMe::_binaryInsertion( ListSequence & sequence, int number )
+{
+	ListSequence::iterator	it = std::lower_bound(sequence.begin(), sequence.end(), number);
+
+	sequence.insert(it, number);
 }
