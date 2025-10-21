841b1cb0933b01e8299cdf846bde419ba3c8c5b0
rmzvr <rmzvr@proton.me>
2025-10-21 20:27:35 +0200

    refactor(ex00): remove redundant error message before usage instruction

--- ex00/sources/main.cpp
+++ ex00/sources/main.cpp
@@ -6,7 +6,6 @@ int main(int argc, char **argv)
 {
 	if (argc < COUNT_OF_ARGUMENTS)
 	{
-		std::cerr << "Error: \n\n";
 		std::cerr << "Usage: " << argv[0] << " <input_file>" << std::endl;
 		return 1;
 	}
